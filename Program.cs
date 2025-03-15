3e1863f1a0ab8157dde3a83b423a037e2e492768
Gustavo Figueiredo Matines <gustavo.martines2003@outlook.com>
2025-03-14 21:29:23 -0300

    foi criado um programa que muda os r pelos l

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string texto;
+string textoLeet;
+
+Console.Clear();
+
+
+Console.WriteLine("Escreve um texto: ");
+texto = Console.ReadLine()!;
+textoLeet = texto.Replace("R", "L")
+                 .Replace("r", "l")
+;
+Console.WriteLine($"Cebolinha: {textoLeet}");
